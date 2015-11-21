44ad506187814e42532654df2bff6ce1237165cb
Nil Portugués Calderó <nilopc@gmail.com>
2015-11-21 18:17:45 +0100

    Added Eloquent Driver

--- src/NilPortugues/Laravel5/JsonApiSerializer/JsonApiSerializer.php
+++ src/NilPortugues/Laravel5/JsonApiSerializer/JsonApiSerializer.php
@@ -10,13 +10,10 @@
  */
 namespace NilPortugues\Laravel5\JsonApiSerializer;
 
-use ErrorException;
-use Illuminate\Database\Eloquent\Model;
 use NilPortugues\Api\JsonApi\JsonApiTransformer;
 use NilPortugues\Laravel5\JsonApiSerializer\Factory\RequestFactory;
 use NilPortugues\Serializer\DeepCopySerializer;
-use ReflectionClass;
-use ReflectionMethod;
+use NilPortugues\Serializer\Drivers\Eloquent\EloquentDriver;
 
 /**
  * Class JsonApiSerializer.
@@ -58,7 +55,6 @@ class JsonApiSerializer extends DeepCopySerializer
 
         return parent::serialize($value);
     }
-
     /**
      * Extract the data from an object.
      *
@@ -68,91 +64,12 @@ class JsonApiSerializer extends DeepCopySerializer
      */
     protected function serializeObject($value)
     {
-        if ($value instanceof \Illuminate\Database\Eloquent\Collection) {
-            $items = [];
-            foreach ($value as &$v) {
-                $items[] = $this->serializeObject($v);
-            }
 
-            return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
-        }
-
-        if ($value instanceof \Illuminate\Contracts\Pagination\Paginator) {
-            $items = [];
-            foreach ($value->items() as &$v) {
-                $items[] = $this->serializeObject($v);
-            }
-
-            return [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
-        }
-
-        if (\is_subclass_of($value, Model::class, true)) {
-            $stdClass = (object) $value->getAttributes();
-            $data = $this->serializeData($stdClass);
-            $data[self::CLASS_IDENTIFIER_KEY] = \get_class($value);
-
-            $methods = $this->getRelationshipMethodsAsPropertyName(
-                $value,
-                \get_class($value),
-                new ReflectionClass($value)
-            );
-
-            if (!empty($methods)) {
-                $data = \array_merge($data, $methods);
-            }
-
-            return $data;
+        $serialized = EloquentDriver::serialize($value);
+        if ($value !== $serialized) {
+            return $serialized;
         }
 
         return parent::serializeObject($value);
-    }
-
-    /**
-     * @param                 $value
-     * @param string          $className
-     * @param ReflectionClass $reflection
-     *
-     * @return array
-     */
-    protected function getRelationshipMethodsAsPropertyName($value, $className, ReflectionClass $reflection)
-    {
-        $methods = [];
-        foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-            if (\ltrim($method->class, '\\') === \ltrim($className, '\\')) {
-                $name = $method->name;
-                $reflectionMethod = $reflection->getMethod($name);
-
-                // Eloquent relations do not include parameters, so we'll be filtering based on this criteria.
-                if (0 == $reflectionMethod->getNumberOfParameters()) {
-                    try {
-                        $returned = $reflectionMethod->invoke($value);
-                        //All operations (eg: boolean operations) are now filtered out.
-                        if (\is_object($returned)) {
-
-                            // Only keep those methods as properties if these are returning Eloquent relations.
-                            // But do not run the operation as it is an expensive operation.
-                            if (false !== \strpos(\get_class($returned), 'Illuminate\Database\Eloquent\Relations')) {
-                                $items = [];
-                                foreach ($returned->getResults() as $model) {
-                                    if (\is_object($model)) {
-                                        $stdClass = (object) $model->getAttributes();
-                                        $data = $this->serializeData($stdClass);
-                                        $data[self::CLASS_IDENTIFIER_KEY] = \get_class($model);
-
-                                        $items[] = $data;
-                                    }
-                                }
-                                if (!empty($items)) {
-                                    $methods[$name] = [self::MAP_TYPE => 'array', self::SCALAR_VALUE => $items];
-                                }
-                            }
-                        }
-                    } catch (ErrorException $e) {
-                    }
-                }
-            }
-        }
-
-        return $methods;
     }
 }
